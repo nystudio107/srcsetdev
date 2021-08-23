a6b06363d4b0a4f6d235104e62b7bead82c2a252
Romuald Brillout <vite-plugin-ssr@brillout.com>
2021-08-23 15:54:27 -0400

    boilerplate Vite w/ vite-plugin-ssr

---
+++ pages/_default/app.ts
@@ -0,0 +1,30 @@
+import { createSSRApp, defineComponent, h } from 'vue'
+import PageWrapper from './PageWrapper.vue'
+import type { PageContext } from './types'
+
+export { createApp }
+
+function createApp(pageContext: PageContext) {
+  const { Page, pageProps } = pageContext
+  const PageWithLayout = defineComponent({
+    render() {
+      return h(
+        PageWrapper,
+        {},
+        {
+          default() {
+            return h(Page, pageProps || {})
+          }
+        }
+      )
+    }
+  })
+
+  const app = createSSRApp(PageWithLayout)
+
+  // We make `pageContext` available in all components as `$pageContext`.
+  // More infos: https://vite-plugin-ssr.com/pageContext-anywhere
+  app.config.globalProperties.$pageContext = pageContext
+
+  return app
+}
